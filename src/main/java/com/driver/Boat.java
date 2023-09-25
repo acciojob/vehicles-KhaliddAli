3081557be4aea0a31af140d345fce669370f92d6
khalid <khalidali967@gmail.com>
2023-09-25 16:26:52 +0530

    added override in boat

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -8,9 +8,11 @@ public class Boat implements WaterVehicle{
         this.name = name;
         this.capacity = capacity;
     }
+    @Override
     public String getVehicleName(){
         return this.name;
     }
+    @Override
     public int getVehicleCapacity(){
         return this.capacity;
     }
